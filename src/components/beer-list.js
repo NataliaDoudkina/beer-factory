1a0d000ee6396b64b18de9d37c7b060d5cb4ebc8
natasobl <natasobl@hotmail.com>
2022-11-12 21:31:34 -0500

    Styling update

--- src/components/beer-list.js
+++ src/components/beer-list.js
@@ -3,34 +3,34 @@ import styles from "../styles";
 
 const BeerList = ({message, beers, beerSearched }) => {
  
-  return (
-    <div>
-        <p>
-            <b>{message}</b>
-        </p>
-        <ul 
-                className="list-group" 
-                style={styles.BeerListStyle}
-        >
-            {beerSearched.length > 0
-            ? beerSearched.map((beer) => (
-                <li className=" pt-4" key={beer.id}>
-                    <Beer beer={beer} />
-                </li>
-                )) : (
-                    beers.map((beer) => (
-                        <li 
-                                className="pt-4" 
-                                key={beer.id}
-                        >
-                            <Beer beer={beer} />
-                        </li>
-                    ))
-                )
-            }
-        </ul>
-    </div>
-  );
+    return (
+        <div>
+            <p>
+                <b>{message}</b>
+            </p>
+            <ul 
+                    className="list-group" 
+                    style={styles.BeerListStyle}
+            >
+                {beerSearched.length > 0
+                ? beerSearched.map((beer) => (
+                    <li className=" pt-4" key={beer.id}>
+                        <Beer beer={beer} />
+                    </li>
+                    )) : (
+                        beers.map((beer) => (
+                            <li 
+                                    className="pt-4" 
+                                    key={beer.id}
+                            >
+                                <Beer beer={beer} />
+                            </li>
+                        ))
+                    )
+                }
+            </ul>
+        </div>
+    );
 };
 
 export default BeerList;
